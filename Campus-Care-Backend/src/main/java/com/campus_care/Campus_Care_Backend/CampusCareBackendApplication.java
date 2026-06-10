66962b9bc4ebe51ff7cab79a7d0d658437646757
Santy0608 <barquerotorress@gmail.com>
2026-06-10 09:44:09 -0600

    feat (Backend): Implementing Backend Structure and initializing backend project

---
+++ Campus-Care-Backend/src/main/java/com/campus_care/Campus_Care_Backend/CampusCareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campus_care.Campus_Care_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusCareBackendApplication.class, args);
+	}
+
+}
---
+++ Campus-Care-Backend/src/test/java/com/campus_care/Campus_Care_Backend/CampusCareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus_care.Campus_Care_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusCareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
